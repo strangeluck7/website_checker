ccb986a81ff81329599285866458e0320061c433
Pete Johnson <strangeluck@gmail.com>
2021-02-18 09:17:11 -0600

    Create website_checker.sh

---
+++ website_checker.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Env variables
+slack_webhook="<ENTER SLACK TOKEN>"
+website="<ENTER WEBSITE ADDRESS>"
+
+function website_update_check() {
+
+    address="$1"
+    name=$(echo "$address" | awk -F'[/:]' '{gsub(/www./,""); print $4}')
+
+    if [ -f "/var/tmp/$name-check1.txt" ]; then
+        echo "Previous Check File Exists - Curling Website again to compare"
+        /usr/bin/curl --insecure "$address" | sed '/Email Us/d' > /var/tmp/"$name"-check2.txt
+
+        echo "Comparing Website Since Last Checked"
+        different_strokes=$(/usr/bin/diff /var/tmp/"$name"-check1.txt /var/tmp/"$name"-check2.txt)
+
+        if [ "$different_strokes" != "" ]; then
+            slackNotifier "$name"
+        fi
+
+        # Cleanup
+        rm /var/tmp/"$name"-check1.txt
+        rm /var/tmp/"$name"-check2.txt
+
+        # Last thing script should do is create new text file to compare for next run
+        /usr/bin/curl --insecure "$1" | sed '/Email Us/d' > /var/tmp/"$name"-check1.txt
+    else
+        # If pacFileRound1 doesn't exist create it
+        /usr/bin/curl --insecure "$1" | sed '/Email Us/d' > /var/tmp/"$name"-check1.txt
+    fi
+
+
+}
+
+function slackNotifier() {
+    echo "New Puppies have been posted!"
+	curl -X POST --data-urlencode "payload={\"attachments\": [{\"color\": \"#FFFF00\", \"title\": \"Dog Notify\", \"title_link\": \"$1\", \"text\": \"A new dog has been added to the website: $address\"}], \"channel\": \"#dogs\", \"username\": \"Dog Notify\", \"icon_emoji\": \":dog2:\"}" "$slack_webhook"
+}
+
+website_update_check "$website"
